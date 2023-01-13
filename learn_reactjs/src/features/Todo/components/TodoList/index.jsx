a4004a3f4c41f2c1e5f0e4bb006d6acbd7e4dcb6
Xuan Nhi <xuannhi27.work@gmail.com>
2023-01-14 01:22:39 +0700

    Active todo

--- learn_reactjs/src/features/Todo/components/TodoList/index.jsx
+++ learn_reactjs/src/features/Todo/components/TodoList/index.jsx
@@ -1,21 +1,37 @@
 /* eslint-disable no-undef */
 import React from 'react';
 import PropTypes from 'prop-types';
-import classNames from 'classnames';
+import classnames from 'classnames';
+import './style.scss';
 
 TodoList.propTypes = {
-    todoList : PropTypes.array,
+    todoList: PropTypes.array,
+    onTodoClick : PropTypes.func,
 };
 
 TodoList.defaultProps = {
-    todoList :[],
+    todoList: [],
+    onTodoClick:null,
 }
 
-function TodoList({todoList}) {
+function TodoList({ todoList, onTodoClick }) {
+    const handleTodoClick = (todo, idx) => {
+        if (!onTodoClick) return;
+
+        onTodoClick(todo, idx);
+    }
+
     return (
-        <ul>
-            {todoList.map(todo => (
-                <li key={todo.id} className={classnames({ completed: todo.status === 'completed' })}>{todo.title}</li>
+        <ul className='todo-list'>
+            {todoList.map((todo,idx) => (
+                <li key={todo.id} className={classnames({
+                    'todo-item': true,
+                    completed: todo.status === 'completed'
+                })}
+                    onClick={()=> handleTodoClick(todo, idx)}
+                >
+                    {todo.title}
+                </li>
             ))}
         </ul>
     );
--- learn_reactjs/src/features/Todo/index.jsx
+++ learn_reactjs/src/features/Todo/index.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React,{useState} from 'react';
 import PropTypes from 'prop-types';
 import TodoList from './components/TodoList';
 TodoFeature.propTypes = {
@@ -6,31 +6,50 @@ TodoFeature.propTypes = {
 };
 
 function TodoFeature (props) {
-    const todoList = [
+    const initTodoList = [
         {
             id: 1,
             title: 'eat',
-            status:'new',
+            status: 'new',
 
         },
         {
             id: 2,
             title: 'code',
-            status:'completed',
+            status: 'completed',
         },
         {
             id: 3,
-            title:'sleep',
-            status:'new',
+            title: 'sleep',
+            status: 'new',
         }
 
-    ]
+    ];
+
+    const [todoList, setTodoList] = useState(initTodoList);
+    
+    const handleTodoClick = (todo, idx) => {
+        
+        //clone current array to the new one
+        const newTodoList = [...todoList]
+
+        console.log(todo, idx)
+        //tongle state
+        newTodoList[idx] = {
+            ...newTodoList[idx],
+            status: newTodoList[idx].status === 'new' ? 'completed' : 'new'
+        }
+        
+        //update todo list 
+        setTodoList(newTodoList)
+    }
     return (
         <div>
             <h1>Todo List</h1>
-            <TodoList todoList={todoList}/>
+            <TodoList todoList={todoList} onTodoClick={handleTodoClick} />
         </div>
-    );
+        );
+        
 }
 
 export default TodoFeature;
